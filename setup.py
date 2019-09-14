805591b20c2e03db727f696339985f427e744a7b
Akhil <55274431+Akhil2149@users.noreply.github.com>
2019-09-14 12:30:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='forpy',
-    version='1.0.0',
+    name='for.py',
+    version='1.0.3',
     description='An async Python API wrapper for the Fortnite API',
     long_description='Powered by fortnitetracker.com. ',
     url='https://github.com/Akhil2149/fortpy',
@@ -10,6 +10,7 @@ setup(
     author_email='akhilaug2003@gmail.com',
     license='MIT',
     keywords=['fortnite, forpy, api-wrapper, async'],
+    python_requires>='3.6',
     packages=find_packages(),
     install_requires=['aiohttp', 'python-box']
 )
